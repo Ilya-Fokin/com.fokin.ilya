18ad18896e53b92d34c0dcf6364ee688e92f95ac
Ilya-Fokin <fokin3349@mail.ru>
2022-02-27 03:55:11 +0400

    Jar package

--- src/com/fokin/ilya/DatesToCronConverter.java
+++ src/com/fokin/ilya/DatesToCronConverter.java
@@ -11,5 +11,6 @@ public interface DatesToCronConverter {
         System.out.println("Имя класса реализации: Cron");
         System.out.println("Пакет: com.fokin.ilya");
         System.out.println("GitHub: https://github.com/Ilya-Fokin/com.fokin.ilya");
+        System.out.println("P.S. Заранее прошу прощения за этот код, задание оказалось сложнее, чем я предполагал... ");
     }
 }
